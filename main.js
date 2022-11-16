943b67a6af240d044fd5d67ee922ae4948c1f453
Sticx <108312079+Sticx@users.noreply.github.com>
2022-11-16 23:36:18 +0200

    fourth

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ function FormValidate(form) {
             const lengthMessage = element.dataset.min_message;
             const reqMessage = element.dataset.req;
             if (reqMessage){
-                this.validReq(reqMessage);
+                this.validReq(reqMessage,element);
             }
             if (passwordMessage) {
                 this.validPassword(passwordMessage);
@@ -36,22 +36,24 @@ function FormValidate(form) {
             }
             if (lengthMessage){
                 this.validLength(lengthMessage);
-                // console.log(element.dataset)
             }
 
         }
     }
-    this.validReq = function (message) {
+    this.validReq = function (message,element) {
         const allInputReq = form.querySelectorAll("input");
         const allInputProp = Array.from(allInputReq).map(element => element.dataset.req);
-        const currentInputLength = Array.from(allInputReq).map(element => element.value.length);
+        const currentInputLength = Array.from(allInputReq).map(element => element.value);
 
+        console.log(allInputReq)
         console.log(allInputProp)
         console.log(currentInputLength)
 
-        if (allInputProp && currentInputLength==null||currentInputLength===""){
-            allInputReq.forEach(item => this.errorTemplate(item, message));
+        if (currentInputLength===null||currentInputLength===""){
+            // allInputReq.forEach(message => this.errorTemplate(message, element));
+            this.errorTemplate(allInputReq,element)
         }
+
     }
     this.validPassword = function (message) {
         const allPasswordElement = form.querySelectorAll("input[type='password']");
@@ -64,14 +66,10 @@ function FormValidate(form) {
 
     this.validLength = function (message) {
         const allInputElement = form.querySelectorAll("input");
-        // const allInputLength = form.querySelector("input[data-min_length]")
         const allInputLength = Array.from(allInputElement).map(element => element.dataset.min_length)
         const currentLength = Array.from(allInputElement).map(element => element.value.length);
-        // const minLength = form.classList.minLength;
 
         if (currentLength<allInputLength){
-            // this.errorTemplate(allInputLength,message);
-            // console.log(allInputLength);
             allInputElement.forEach(item =>this.errorTemplate(item,message))
         }
     }
@@ -80,74 +78,11 @@ function FormValidate(form) {
         const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
         const mail = form.querySelector("input[name='email']");
 
-        // console.log(mail.value);
         if (!EMAIL_REGEXP.test(mail.value)){
             this.errorTemplate(mail,message);
         }
 
     }
-    // this.onInput = function (message) {
-    //     const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
-    //     const mail = form.querySelector("input[name='email']");
-    //     if (EMAIL_REGEXP.test(value)) {
-    //         mail.style.borderColor = 'green';
-    //     } else {
-    //         mail.style.borderColor = 'red';
-    //        // const emailMessage = mail.dataset.email;
-    //        // mail.forEach(item =>this.errorTemplate(emailMessage)); ERROR
-    //         mail.forEach(item=>this.errorTemplate(item,message));
-    //     }
-    // }
-
-
-    // const email = form.querySelector("input[name='email']");
-    // const emailError = email.dataset.email;
-    //
-    // email.addEventListener('input', function (event) {
-    //     // Каждый раз, когда пользователь что-то вводит,
-    //     // мы проверяем, являются ли поля формы валидными
-    //
-    //     if (email.validity.valid) {
-    //         // Если на момент валидации какое-то сообщение об ошибке уже отображается,
-    //         // если поле валидно, удаляем сообщение
-    //         emailError.textContent = ''; // Сбросить содержимое сообщения
-    //         emailError.className = 'error'; // Сбросить визуальное состояние сообщения
-    //     } else {
-    //         // Если поле не валидно, показываем правильную ошибку
-    //        this.errorTemplate();
-    //     }
-    // });
-    //
-    // form.addEventListener('input', function (event) {
-    //     // Если поле email валдно, позволяем форме отправляться
-    //
-    //     if(!email.validity.valid) {
-    //         // Если поле email не валидно, отображаем соответствующее сообщение об ошибке
-    //         this.errorTemplate();
-    //         // Затем предотвращаем стандартное событие отправки формы
-    //         event.preventDefault();
-    //     }
-    // });
-    //
-    // // function showError() {
-    // //     if(email.validity.valueMissing) {
-    // //         // Если поле пустое,
-    // //         // отображаем следующее сообщение об ошибке
-    // //         emailError.textContent = 'You need to enter an e-mail address.';
-    // //     } else if(email.validity.typeMismatch) {
-    // //         // Если поле содержит не email-адрес,
-    // //         // отображаем следующее сообщение об ошибке
-    // //         emailError.textContent = 'Entered value needs to be an e-mail address.';
-    // //     } else if(email.validity.tooShort) {
-    // //         // Если содержимое слишком короткое,
-    // //         // отображаем следующее сообщение об ошибке
-    // //         emailError.textContent = `Email should be at least ${ email.minLength } characters; you entered ${ email.value.length }.`;
-    // //     }
-    // //
-    // //     // Задаём соответствующую стилизацию
-    // //     emailError.className = 'error active';
-    // // }
-
     this.errorTemplate = function (element, message) {
         const parent = element.closest(`.${_parentItemClass}`);
         if (!parent.classList.contains(_errorWrapperClass)) {
