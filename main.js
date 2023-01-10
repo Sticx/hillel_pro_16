93f468471a1d25e3364a8a1b5ff57aadccd9ec46
Sticx <108312079+Sticx@users.noreply.github.com>
2023-01-11 00:15:04 +0200

    five

--- main.js
+++ main.js
@@ -8,92 +8,74 @@ function FormValidate(form) {
         event.preventDefault();
         this.checkFormElement();
     })
-    
-    this.clearElementError = function (element) {
-        const parent = element.closest(`.${_parentItemClass}`)
-        if (parent !== null && parent.classList.contains(_errorWrapperClass)=== true){
-            parent.classList.remove(_errorWrapperClass);
-            parent.querySelector(`.${_errorItemClass}`).remove()
-        }
-    }
 
     this.checkFormElement = function () {
         for (let i = 0; i < _elements.length; i++) {
             const element = _elements[i];
-            this.clearElementError(element);
+            this.clearFormError(element);
             const passwordMessage = element.dataset.password;
-            const emailMessage = element.dataset.email;
-            const lengthMessage = element.dataset.min_message;
-            const reqMessage = element.dataset.req;
-            if (reqMessage){
-                this.validReq(reqMessage,element);
-
-                if (element.value===''){
-                    this.errorTemplate(element,reqMessage);
-                }
-            }
             if (passwordMessage) {
                 this.validPassword(passwordMessage);
             }
-            if (emailMessage){
-                this.validEmail(emailMessage);
+            const emailMessage = element.dataset.email;
+            if (emailMessage) {
+                this.validEmail(element, emailMessage);
             }
-            if (lengthMessage){
-                this.validLength(lengthMessage);
+            const requiredMessage = element.dataset.req;
+            if (requiredMessage) {
+                this.required(element, requiredMessage);
             }
-
+            const minLength = element.dataset.min_length;
+            const minLengthMessage = element.dataset.min_message;
+            if (minLengthMessage) {
+                this.minLength(element, minLengthMessage.replace("N", minLength));
+            }
         }
     }
-    this.validReq = function (message) {
-        // const allInputReq = form.querySelectorAll("input");
-        // // const allInputProp = Array.from(allInputReq).map(element => element.dataset.req);
-        // // const currentInputLength = Array.from(allInputReq).map(element => element.value);
-        //
-        // console.log(typeof allInputReq.dataset)
-        // // console.log(allInputProp)
-        // // console.log(currentInputLength)
-        //
-        // if (allInputReq.value===''){
-        //     // allInputReq.forEach(element => this.errorTemplate(message, element));
-        //     this.errorTemplate(allInputReq,message)
-        // }
-        const inputCheck = form.querySelectorAll("input[type='checkbox']");
-        // const currenCheck = Array.from(inputCheck).map(element => element.value);
-        console.log(inputCheck)
-        // console.log(currenCheck)
-        if (!inputCheck.checked){
-            this.errorTemplate(inputCheck,message)
-        }
 
+    this.clearFormError = function (item) {
+        const parent = item.closest(`.${_parentItemClass}`);
+        const checkParent = parent !== null && parent.classList.contains(_errorWrapperClass) === true;
+        if (checkParent) {
+            parent.classList.remove(_errorWrapperClass);
+            parent.querySelector(`.${_errorItemClass}`).remove();
+        }
     }
+
     this.validPassword = function (message) {
         const allPasswordElement = form.querySelectorAll("input[type='password']");
         const valueArr = Array.from(allPasswordElement).map(element => element.value);
 
         if (valueArr[0] !== valueArr[1]) {
-            allPasswordElement.forEach(item => this.errorTemplate(item, message));
+            allPasswordElement.forEach(item => this.errorTemplate(item, message))
         }
     }
 
-    this.validLength = function (message) {
-        const allInputElement = form.querySelectorAll("input");
-        const allInputLength = Array.from(allInputElement).map(element => element.dataset.min_length)
-        const currentLength = Array.from(allInputElement).map(element => element.value.length);
-
-        if (currentLength<allInputLength){
-            allInputElement.forEach(item =>this.errorTemplate(item,message))
+    this.validEmail = function (item, message) {
+        const emailElement = form.querySelector("input[name='email']");
+        const emailElValue = emailElement.value.toLowerCase();
+        const emailRegExp = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        if (!emailElValue.match(emailRegExp)) {
+            this.errorTemplate(item, message);
         }
     }
 
-    this.validEmail = function (message){
-        const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
-        const mail = form.querySelector("input[name='email']");
+    this.required = function (item, message) {
+        const invalidString = item.value.length === 0;
+        const invalidCheckBox = item.type === "checkbox" && item.checked === false;
+        if (invalidString || invalidCheckBox) {
+            this.errorTemplate(item, message);
+        }
+    }
 
-        if (!EMAIL_REGEXP.test(mail.value)){
-            this.errorTemplate(mail,message);
+    this.minLength = function (item, message) {
+        const validLength = item.value.length >= item.dataset.min_length;
+        if (!validLength) {
+            this.errorTemplate(item, message);
         }
 
     }
+
     this.errorTemplate = function (element, message) {
         const parent = element.closest(`.${_parentItemClass}`);
         if (!parent.classList.contains(_errorWrapperClass)) {
